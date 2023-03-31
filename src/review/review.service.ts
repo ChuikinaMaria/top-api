d6a872eafb63504ee4026004d616139ae3d64e24
Maria Chuikina <4uikina@gmail.com>
2023-03-31 10:57:07 +0200

    deleteByProductId service

--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -3,6 +3,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model, Types } from 'mongoose';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { ReviewModel, ReviewDocument } from './review.model';
+import { DeleteResult } from 'mongodb';
 
 @Injectable()
 export class ReviewService {
@@ -22,6 +23,12 @@ export class ReviewService {
 	async findProductId(productId: string): Promise<ReviewDocument[]> {
 		return this.reviewModel
 			.find({ productId: new Types.ObjectId(productId) })
+			.exec();
+	}
+
+	async deleteByProductId(productId: string): Promise<DeleteResult> {
+		return this.reviewModel
+			.deleteMany({ productId: new Types.ObjectId(productId) })
 			.exec();
 	}
 }
